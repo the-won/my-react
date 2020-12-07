61ca2f4a5cf2537e75052a702dc752608525b7c2
the-won <bonav00@naver.com>
2020-12-07 15:48:28 +0900

    Update webpack.config.js
    docs add to rules

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,37 @@ module.exports = {
                     'react-refresh/babel',
                 ],
             },
-        }],
+        },
+        {
+        	test: /\.css$/,
+            use: [{
+                    loader: 'style-loader'
+                },
+                {
+                    loader: 'css-loader'
+                }
+            ]
+        },
+        {
+            test: /\.(ttf|eot|svg|woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            use: [{
+                loader: 'file-loader'
+            }]
+        },
+        {
+        	test: /.(sass|scss)$/,
+        	use: [
+        	{ loader: 'style-loader' },
+        
+        	]
+        },
+        {
+            test: /\.(png|jpg|gif)$/,
+            use: [{
+                loader: 'file-loader'
+            }]
+        }
+        ],
     },
 
     plugins: [
